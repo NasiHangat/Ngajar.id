2259154dbf337ff8ee299bcdd3b81c132124af89
Azis4215 <krnshinigami@gmail.com>
2025-06-02 21:32:28 +0700

    Edit typo title

--- donasi.php
+++ donasi.php
@@ -21,7 +21,7 @@ $result_riwayat = $conn->query($sql_riwayat);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Donasi</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;300;400;500;600;700;900&display=swap" rel="stylesheet">
     <script>
