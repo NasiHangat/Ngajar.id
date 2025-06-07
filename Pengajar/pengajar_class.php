8483bcde8a9e14f79bee01d6efbee676d3cd243c
Rizkek <148307907+Rizkek@users.noreply.github.com>
2025-06-08 01:16:42 +0700

    Update pengajar_class.php

--- Pengajar/pengajar_class.php
+++ Pengajar/pengajar_class.php
@@ -3,22 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Ngajar.ID</title>
+    <title>Kelas - Ngajar.ID</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../styles/warna.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;300;400;500;600;700;900&display=swap" rel="stylesheet">
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    fontFamily: {
-                        roboto: ['"Roboto Slab"', 'serif'],
-                    }
-                }
-            }
-        }
-    </script>
 </head>
 <body class="bg-white-50 font-roboto">
     <div class="flex flex-col min-h-screen">
