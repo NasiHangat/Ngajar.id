5fd7144a47048e1d993ae2c170b7afbbe82635cd
Azis4215 <krnshinigami@gmail.com>
2025-06-15 23:36:35 +0700

    FINALLL

--- ngajar_id.sql
+++ ngajar_id.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Jun 2025 pada 13.28
+-- Waktu pembuatan: 15 Jun 2025 pada 17.59
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -59,13 +59,6 @@ CREATE TABLE `donasi` (
   `tanggal` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `donasi`
---
-
-INSERT INTO `donasi` (`donasi_id`, `nama`, `jumlah`, `tanggal`) VALUES
-(3, 'asd', 1000000, '2025-06-14 20:03:46');
-
 -- --------------------------------------------------------
 
 --
@@ -81,16 +74,6 @@ CREATE TABLE `kelas` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data untuk tabel `kelas`
---
-
-INSERT INTO `kelas` (`kelas_id`, `pengajar_id`, `judul`, `deskripsi`, `status`, `created_at`) VALUES
-(5, 22, 'Pemrograman Web', 'PEMWEB 1', 'aktif', '2025-06-14 07:36:56'),
-(6, 22, 'Anjay', 'mabar', 'aktif', '2025-06-14 08:48:15'),
-(7, 20, 'Pemrograman Web', 'AWWWW', 'aktif', '2025-06-14 18:34:23'),
-(8, 22, 'aaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aktif', '2025-06-15 08:19:52');
-
 -- --------------------------------------------------------
 
 --
@@ -103,16 +86,6 @@ CREATE TABLE `kelas_peserta` (
   `kelas_id` int(11) DEFAULT NULL,
   `tanggal_daftar` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `kelas_peserta`
---
-
-INSERT INTO `kelas_peserta` (`id`, `siswa_id`, `kelas_id`, `tanggal_daftar`) VALUES
-(1, 23, 5, NULL),
-(2, 23, 6, NULL),
-(3, 23, 7, '2025-06-15 12:10:32'),
-(4, 23, 8, '2025-06-15 15:20:03');
 
 -- --------------------------------------------------------
 
@@ -129,14 +102,6 @@ CREATE TABLE `materi` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `deskripsi` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `materi`
---
-
-INSERT INTO `materi` (`materi_id`, `kelas_id`, `judul`, `tipe`, `file_url`, `created_at`, `deskripsi`) VALUES
-(10, 7, 'Tes Admin', '', '../uploads/materi/1749926083_1284821.jpg', '2025-06-14 18:34:43', NULL),
-(11, 5, 'Tes Admin', 'pdf', '../uploads/materi/1749964307_1284821.jpg', '2025-06-15 05:11:47', NULL);
 
 -- --------------------------------------------------------
 
@@ -155,14 +120,18 @@ CREATE TABLE `modul` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `modul`
+-- Struktur dari tabel `modul_user`
 --
 
-INSERT INTO `modul` (`modul_id`, `judul`, `deskripsi`, `file_url`, `tipe`, `token_harga`, `dibuat_oleh`, `created_at`) VALUES
-(7, 'cihuy', 'aaaaa', '../uploads/modul/1749922966_684db4960d061.jpg', '', 1000, 21, '2025-06-14 17:42:46'),
-(11, 'tess', 'aaaaa', '../uploads/modul/1749923051_684db4eb5cc85.docx', '', 2222, 21, '2025-06-14 17:44:11'),
-(13, 'aw', 'hiyaaaaaaaaaa', '', '', 10, 21, '2025-06-15 05:56:11');
+CREATE TABLE `modul_user` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `modul_id` int(11) DEFAULT NULL,
+  `tanggal_beli` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -187,6 +156,7 @@ CREATE TABLE `token_log` (
   `user_id` int(11) DEFAULT NULL,
   `modul_id` int(11) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
+  `aksi` varchar(20) DEFAULT NULL,
   `tanggal` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -246,12 +216,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `role`, `status`, `created_at`) VALUES
-(19, 'A', 'D@gmail.com', '$2y$10$5VffwNGmfK.BoouYo9.fRO9bHdTO5yVQVFPh8TfycncQuODsOknSq', 'murid', 'aktif', '2025-06-04 16:22:19'),
-(20, 'asd', 'a@gmail.com', '$2y$10$JM5wbLRDkaxwFlxIkPIyaO7g36UbyieO5CVXjQwDTg48xKD7CUhm6', 'pengajar', 'aktif', '2025-06-11 07:30:53'),
-(21, 'Azis', 'mamanganteng@gmail.com', '$2y$10$lLjwFbwBVqCxB1hObBrvzuVbyfyHfZPvECUQrAT9ygpe9zMaygXNa', 'admin', 'aktif', '2025-06-11 09:57:45'),
-(22, 'Azis', 'muhammadabdulazis747@gmail.com', '$2y$10$oxto5WrwdXmoT.acFoXp.ubGJ9iS8IPQgxycpl.OVPKYvYRxXTQYq', 'pengajar', 'aktif', '2025-06-14 07:33:43'),
-(23, 'Budi', 'anjing@gmail.com', '$2y$10$B0EpShwqRtiho0pXGIKa6OXPMkDluLQfkSW74Fjp.eUdDA11tclza', 'murid', 'aktif', '2025-06-14 07:53:30'),
-(24, 'asep', 'asep@gmail.com', '$2y$10$OLRhD/ANnZfSsHZzXU3a/usFEulYM4V77WykIKH0gSHsC5Bqo.bDi', 'pengajar', 'aktif', '2025-06-14 13:03:57');
+(21, 'Azis', 'mamanganteng@gmail.com', '$2y$10$lLjwFbwBVqCxB1hObBrvzuVbyfyHfZPvECUQrAT9ygpe9zMaygXNa', 'admin', 'aktif', '2025-06-11 09:57:45');
 
 --
 -- Indexes for dumped tables
@@ -291,6 +256,12 @@ ALTER TABLE `materi`
 ALTER TABLE `modul`
   ADD PRIMARY KEY (`modul_id`),
   ADD KEY `dibuat_oleh` (`dibuat_oleh`);
+
+--
+-- Indeks untuk tabel `modul_user`
+--
+ALTER TABLE `modul_user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `token`
@@ -334,37 +305,43 @@ ALTER TABLE `donasi`
 -- AUTO_INCREMENT untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `kelas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `kelas_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas_peserta`
 --
 ALTER TABLE `kelas_peserta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `materi`
 --
 ALTER TABLE `materi`
-  MODIFY `materi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `materi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `modul`
 --
 ALTER TABLE `modul`
-  MODIFY `modul_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `modul_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `modul_user`
+--
+ALTER TABLE `modul_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `token_log`
 --
 ALTER TABLE `token_log`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `topup`
 --
 ALTER TABLE `topup`
-  MODIFY `topup_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `topup_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
