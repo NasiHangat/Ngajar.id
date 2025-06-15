1844473dbd87096055bb68b3cc11c9c507349a5c
Rizkek <riskiinferno@gmail.com>
2025-06-15 13:22:46 +0700

    Update admin_dashboard.php

--- Admin/admin_dashboard.php
+++ Admin/admin_dashboard.php
@@ -181,7 +181,7 @@ while ($row = mysqli_fetch_assoc($result_top_murid)) {
     
     <section class="py-10 px-4">
                 <div class="max-w-4xl mx-auto">
-                    <h2 class="text-2xl font-bold text-center text-teal-50 mb-8">Peringkat Teratas Pembelian Token</h2>
+                    <h2 class="text-2xl font-bold text-center text-teal-500 mb-8">Peringkat Teratas Pembelian Token</h2>
                     <div class="flex justify-center items-end gap-4 md:gap-6">
                         <!-- Peringkat 2 -->
                         <div class="text-center w-1/3">
